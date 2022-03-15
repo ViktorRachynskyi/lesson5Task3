8141cc789a472d3ae04be1166ff53fa526063416
Viktor <viktor.rachynskyi@asquad.io>
2022-03-15 20:42:17 +0200

    create Drag&Drop test

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 public class DragAndDropTest {
+
+    @Test
+    void dropAndDragTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-b").shouldHave(text("A"));
+    }
 }
